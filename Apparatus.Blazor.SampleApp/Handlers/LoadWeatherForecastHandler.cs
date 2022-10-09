43cbf520eded9735929f1d6de9ee7810ff6c6e25
quantumqube <quantumqube@outlook.com>
2022-10-09 13:10:57 +0400

    WeatherForecast example added

---
+++ Apparatus.Blazor.SampleApp/Handlers/LoadWeatherForecastHandler.cs
@@ -0,0 +1,36 @@
+﻿using Apparatus.Blazor.SampleApp.Actions;
+using Apparatus.Blazor.SampleApp.States;
+using Apparatus.Blazor.State.Contracts;
+using System.Net.Http.Json;
+
+
+namespace Apparatus.Blazor.SampleApp.Handlers
+{
+    public class LoadWeatherForecastHandler : IActionHandler<LoadWeatherForecast>
+    {
+        private IStore<WeatherForecastTableState> _tableStore;
+        private HttpClient _httpClient;
+
+        public LoadWeatherForecastHandler(IStore<WeatherForecastTableState> tableStore, HttpClient httpClien)
+        {
+            _tableStore = tableStore;
+            _httpClient = httpClien;
+        }
+        public async Task Handle(LoadWeatherForecast action)
+        {
+            var newState = _tableStore.State;
+
+            newState.Loading = true;
+
+            await _tableStore.SetState(newState);
+
+            newState.Forecasts = await _httpClient.GetFromJsonAsync<List<WeatherForecast>>("sample-data/weather.json") ?? new();
+
+            newState.Loading = false;
+
+            newState.Initialized = true;
+
+            await _tableStore.SetState(newState);
+        }
+    }
+}
---
+++ Apparatus.Blazor.SampleApp/States/WeatherForecastTableState.cs
@@ -0,0 +1,24 @@
+﻿using Apparatus.Blazor.State.Contracts;
+
+namespace Apparatus.Blazor.SampleApp.States
+{
+    public class WeatherForecastTableState : IState
+    {
+        public List<WeatherForecast> Forecasts { get; set; } = new();
+
+        public bool Loading { get; set; }
+
+        public bool Initialized { get; set; }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
