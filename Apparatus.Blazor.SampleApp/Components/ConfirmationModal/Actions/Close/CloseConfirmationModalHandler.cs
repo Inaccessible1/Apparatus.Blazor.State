b2049b1a0c38d2cf4841120f55697c19da8e250b
quantumqube <quantumqube@outlook.com>
2022-11-11 16:56:15 +0400

    Additional examples added

---
+++ Apparatus.Blazor.SampleApp/Components/ConfirmationModal/Actions/Close/CloseConfirmationModalHandler.cs
@@ -0,0 +1,22 @@
+﻿using Apparatus.Blazor.State.Contracts;
+
+namespace Apparatus.Blazor.SampleApp.Components.ConfirmationModal.Actions.Close
+{
+    public class CloseConfirmationModalHandler : IActionHandler<CloseConfirmationModal>
+    {
+        private IStore<ConfirmationModalState> _store;
+
+        public CloseConfirmationModalHandler(IStore<ConfirmationModalState> store)
+        {
+            _store = store; 
+        }
+        public Task Handle(CloseConfirmationModal action)
+        {
+            var state = _store.State;
+
+            state.Display = false;
+
+            return _store.SetState(state);
+        }
+    }
+}
